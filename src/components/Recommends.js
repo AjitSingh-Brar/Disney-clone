afddaad51dfed1870938f175b7d72b5ecd3a6356
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-04-27 21:12:10 -0400

    CHNG: Trending, NewDisney, Recommended and Originals components improved.

--- src/components/Recommends.js
+++ src/components/Recommends.js
@@ -1,43 +1,26 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectRecommend } from "../features/movie/movieSlice";
 
 const Recommends = (props) => {
+  const movies = useSelector(selectRecommend);
+
+  console.log(movies);
+
   return (
     <Container>
       <h4>Recommended for you</h4>
       <Content>
-        <Wrap>
-          <Link to="/">
-            <img
-              src="https://wfuogb.com/wp-content/uploads/2021/03/WandaVision_logo.png"
-              alt=""
-            />
-          </Link>
-        </Wrap>
-        <Wrap>
-          <Link to="/">
-            <img
-              src="https://wfuogb.com/wp-content/uploads/2021/03/WandaVision_logo.png"
-              alt=""
-            />
-          </Link>
-        </Wrap>
-        <Wrap>
-          <Link to="/">
-            <img
-              src="https://wfuogb.com/wp-content/uploads/2021/03/WandaVision_logo.png"
-              alt=""
-            />
-          </Link>
-        </Wrap>
-        <Wrap>
-          <Link to="/">
-            <img
-              src="https://wfuogb.com/wp-content/uploads/2021/03/WandaVision_logo.png"
-              alt=""
-            />
-          </Link>
-        </Wrap>
+        {movies &&
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              {movie.id}
+              <Link to={"/detail/" + movie.id}>
+                <img src={movie.cardImg} alt={movie.title} />
+              </Link>
+            </Wrap>
+          ))}
       </Content>
     </Container>
   );
